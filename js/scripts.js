3fc910f9bd554bdaf346dc69d113176a4c216593
Angie wambui <60581052+angiewambui@users.noreply.github.com>
2020-02-25 07:45:16 -0500

    added submit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
     $("#development-image").click(function(){
-      $("#development-image").toggle('1500').hide('1000');
+      $("#development-image").slideDown('1500').hide('1000');
       $("#development").show('1500');
     });
     $("#development").click(function(){
-      $("#development").toggle('1500');
+      $("#development").slideUp('1500');
       $("#development-image").slideDown('1500');
     });
   });
   
   $(document).ready(function(){
     $("#design-image").click(function(){
-      $("#design-image").toggle'1500').hide('1000');
+      $("#design-image").slideDown('1500').hide('1000');
       $("#design").show('1500');
     });
     $("#design").click(function(){
-      $("#design").toggle('1500');
-      $("#design-image").toggle('1500');
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
     });
   });
   
   $(document).ready(function(){
     $("#product-image").click(function(){
-      $("#product-image").toggle('1500').hide('1000');
+      $("#product-image").slideDown('1500').hide('1000');
       $("#product").show('1500');
     });
     $("#product").click(function(){
@@ -30,3 +30,18 @@ $(document).ready(function(){
       $("#product-image").slideDown('1500');
     });
 });
+$(document).ready(function(){
+    $("detailsform").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#firstname").val();
+      var email = $("input#email").val();
+      var message = $("textarea#comment").val();
+      if ($("input#firstname").val() && $("input#email").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+    });
+  }); 
+  
